7924ae922e1761c9df38763a2b9453dcb73cf17a
FRANK KWIZERA MUGWANEZA <158175603+programmer-233@users.noreply.github.com>
2025-01-30 00:44:09 +0000

    Game Loop

--- src/Window.java
+++ src/Window.java
@@ -1,6 +1,9 @@
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class Window extends JFrame implements Runnable{
+
+    Graphics2D g2;
 
     public Window() {
         this.setSize(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
@@ -8,12 +11,29 @@ public class Window extends JFrame implements Runnable{
         this.setResizable(false);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        g2 = (Graphics2D)this.getGraphics();
+    }
+    // Delta Time
+    public void update(double dt){
+        g2.setColor(new Color(120, 177, 114));
+        g2.fillRect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+
 
     }
-
     public void run(){
+        double lastFrameTime = 0.0;
         while (true) {
+            double time = Time.getTime();
+            double deltaTime = time - lastFrameTime;
+            lastFrameTime = time;
 
+            update(deltaTime);
+
+            try {
+                Thread.sleep(30);
+            }catch (Exception e){
+
+            }
         }
 
     }
